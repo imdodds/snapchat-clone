92a92bb6e77ba0b8387e2bc8e45f1bfee60bc83f
Ian Dodds <ianmitchelldodds@gmail.com>
2023-03-09 11:47:00 -0500

    Add WebcamCapture component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import WebcamCapture from './components/WebcamCapture';
 
 function App() {
   return (
     <div className="App">
       <h1>Snapchat Clone</h1>
+      <WebcamCapture />
     </div>
   );
 }
