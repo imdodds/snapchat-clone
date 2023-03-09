d01235838f3189ac69d50e119f969968d62c57ba
Ian Dodds <ianmitchelldodds@gmail.com>
2023-03-09 17:28:01 -0500

    Add Preview toolbar and sendPost function

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,14 +1,18 @@
-import { Close } from '@mui/icons-material';
 import React, { useEffect } from 'react'
+import { AttachFile, Close, Create, Crop, MusicNote, Note, Send, TextFields, TimerOutlined } from '@mui/icons-material';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { resetCameraImage, selectCameraImage } from '../features/cameraSlice';
+import { v4 as uuid } from 'uuid';
+import firebase from 'firebase/compat/app';
+import { db, storage } from '../firebase';
 import '../styles/Preview.css';
+
 
 function Preview() {
 
   const cameraImage = useSelector(selectCameraImage);
-  
+
   const navigate = useNavigate();
 
   const dispatch = useDispatch();
@@ -23,13 +27,62 @@ function Preview() {
     dispatch(resetCameraImage());
   }
 
+  const sendPost = () => {
+
+    const id = uuid();
+    const uploadTask = storage
+      .ref(`posts/${id}`)
+      .putString(cameraImage, 'data_url');
+
+    uploadTask.on(
+      'state_changed',
+      null,
+      (error) => {
+        // ERROR function
+        console.log(error)
+      },
+      () => {
+        // COMPLETE function
+        storage
+          .ref('posts')
+          .child(id)
+          .getDownloadURL()
+          .then((url) => {
+            db.collection('posts').add({
+              imageUrl: url,
+              username: 'Ian',
+              read: false,
+              timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+            });
+            navigate('/chats');
+        });
+      }
+    );
+  };
+
   return (
     <div className='preview'>
       <Close
         onClick={closePreview}
         className='preview__close'
       />
+      <div className="preview__toolbarRight">
+        <TextFields />
+        <Create />
+        <Note />
+        <MusicNote />
+        <AttachFile />
+        <Crop />
+        <TimerOutlined />
+      </div>
       <img src={cameraImage} alt='' />
+      <div onClick={sendPost} className="preview__footer">
+        <h2>Send Now</h2>
+        <Send
+          fontSize='small'
+          className='preview__sendIcon'
+        />
+      </div>
     </div>
   )
 }
