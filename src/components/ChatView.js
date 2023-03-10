edea6285bf4a9463d5c64147e57db317a5d63423
Ian Dodds <ianmitchelldodds@gmail.com>
2023-03-09 21:44:43 -0500

    Add ChatView timer

--- src/components/ChatView.js
+++ src/components/ChatView.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { selectSelectedImage } from '../features/appSlice';
+import { CountdownCircleTimer } from 'react-countdown-circle-timer';
 import '../styles/ChatView.css';
 
 function ChatView() {
@@ -23,6 +24,25 @@ function ChatView() {
   return (
     <div className='chatView'>
       <img src={selectedImage} onClick={exit} alt="" />
+
+      <div className="chatView__timer">
+        <CountdownCircleTimer
+          isPlaying
+          duration={10}
+          strokeWidth={6}
+          size={50}
+          colors={['#004777', '#f7b801', '#a30000']}
+          colorsTime={[10, 6, 3]}
+        >
+          {({ remainingTime }) => {
+            if (remainingTime === 0) {
+              exit();
+            }
+
+            return remainingTime
+          }}
+        </CountdownCircleTimer>
+      </div>
     </div>
   )
 }
